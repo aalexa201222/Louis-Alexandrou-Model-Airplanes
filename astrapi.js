f2769d8540e8ccc41e7d1be9c2fd2f1fdc06cba4
aalexa02 <63289407+aalexa02@users.noreply.github.com>
2022-08-10 12:49:52 +0300

    Update astrapi.js

--- astrapi.js
+++ astrapi.js
@@ -13,41 +13,41 @@ document.addEventListener("DOMContentLoaded",function(event){
     
     console.log("saini")
     
-    var tl= new TimelineMax({onUpdate:updatePercentage}); //{onUpdate:updatePercentage} for scrollbased animations
-    var tl2= new TimelineMax({onUpdate:updatePercentage});
-    const controller= new ScrollMagic.Controller();
-    
-    
-    
-    tl.from("#pic1",0.5,{x:-200, opacity:0});
-    tl.from("#speech",1,{x:200, opacity:0});
-    tl2.from("#pic2",1,{x:-200, opacity:0});
-    tl2.from("#speech2",1,{x:200, opacity:0});
+        var tl= new TimelineMax({onUpdate:updatePercentage}); //{onUpdate:updatePercentage} for scrollbased animations
+        var tl2= new TimelineMax({onUpdate:updatePercentage});
+        const controller= new ScrollMagic.Controller();
+        
+        
+        if(window.screen.width>500){ 
+            tl.from("#pic1",0.5,{x:-200, opacity:0});
+            tl.from("#speech",1,{x:200, opacity:0});
+            tl2.from("#pic2",1,{x:-200, opacity:0});
+            tl2.from("#speech2",1,{x:200, opacity:0});
 
-    
-        console.log("first ");
-        const scene = new ScrollMagic.Scene({
-            triggerElement: ".triker",          //possition to trigger element
-        })
-    
-            .setTween(tl)
-             .addTo(controller);
-    
-        const scene2 = new ScrollMagic.Scene({
-            triggerElement: ".triker2",          //possition to trigger element
-         })
             
-            .setTween(tl2)
-             .addTo(controller);
+                console.log("first ");
+                const scene = new ScrollMagic.Scene({
+                    triggerElement: ".triker",          //possition to trigger element
+                })
+            
+                    .setTween(tl)
+                    .addTo(controller);
+            
+                const scene2 = new ScrollMagic.Scene({
+                    triggerElement: ".triker2",          //possition to trigger element
+                })
+                    
+                    .setTween(tl2)
+                    .addTo(controller);
 
-        function updatePercentage(){
-                // if(window.innerWidth<=600)
-                console.log("window.innerWidth ");
-                tl.progress();
-            // console.log(tl.progress());
-    
+                function updatePercentage(){
+                        // if(window.innerWidth<=600)
+                        console.log("window.innerWidth ");
+                        tl.progress();
+                    // console.log(tl.progress());
+            
+            }
     }
-    
     });
     
     
